25cd22cb5a95b8365b9fd0da19de992a29057c67
Nazmos27 <nazmos789@gmail.com>
2023-05-04 04:30:00 +0600

    sending data based on dynamic route

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 5000
 
 
 const test = require('./data/test.json')
+const chefs_data = require('./data/chefs_data.json')
+const recipe_details = require('./data/recipe_details.json')
 
 app.use(cors())
 
@@ -14,6 +16,20 @@ app.get('/', (req,res) => {
 
 app.get('/test',(req,res)=> {
     res.send(test)
+})
+
+app.get('/chefs_data', (req,res) => {
+    res.send(chefs_data)
+})
+
+app.get('/recipe_details' , (req,res) => {
+    res.send(recipe_details)
+})
+
+app.get('/recipe_details/:id',(req,res) => {
+    const id = req.params.id;
+    res.send(recipe_details.find(item => item.id == id))//if i use filter here,it returns an array which in incovenient to fetch and load,so have to use find,find return that element match the condition
+    
 })
 
 app.listen(port , () => {
